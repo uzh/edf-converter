c2d156f5a569401475353cf033b4465d83e1bcfd
unknown <etter@EtterDev02.ad.econ.private>
2013-04-16 15:58:30 +0200

    pupil size wrong indexed, fixed

--- edf2mat_example.m
+++ edf2mat_example.m
@@ -1,19 +1,19 @@
 %% Example 1 for how to use the Edf Converter
 
 %% Converting the EDF File and saving it as a Matlab File
-edf = Edf2Mat('eyedata.edf');
+edf0 = Edf2Mat('eyedata.edf');
 
 %% The edf Variable now holds all information
 % lets display it:
 
-disp(edf);
+disp(edf0);
 
 %% And how about just plot it?
-plot(edf);
+plot(edf0);
 
 %% Of course you can also plot in your own style:
 figure();
-plot(edf.Samples.posX(end - 2000:end), edf.Samples.posY(end - 2000:end), 'o');
+plot(edf0.Samples.posX(end - 2000:end), edf0.Samples.posY(end - 2000:end), 'o');
 
 
 
@@ -21,26 +21,31 @@ plot(edf.Samples.posX(end - 2000:end), edf.Samples.posY(end - 2000:end), 'o');
 %% Example 2 for how to use the Edf Converter
 
 %% Converting the EDF File and saving it as a Matlab File
-edf = Edf2Mat('fMRI_Results_sub_025_270712EYE25r1.edf');
+edf1 = Edf2Mat('fMRI_Results_sub_025_270712EYE25r1.edf');
 
 %% The edf Variable now holds all information
 % lets display it:
 
-disp(edf);
+disp(edf1);
 
 %% And how about just plot it?
-plot(edf);
+plot(edf1);
 
 %% Of course you can also plot in your own style:
 figure();
-plot(edf.Samples.posX(end - 2000:end), edf.Samples.posY(end - 2000:end), 'o');
+plot(edf1.Samples.posX(end - 2000:end), edf1.Samples.posY(end - 2000:end), 'o');
 
 %% Plot the progress of the pupil size
 
 figure();
-plot(edf.Samples.pa(1:100));
+plot(edf1.Samples.pa(2, end - 500:end));
 
 %% Example 2 for how to use the Edf Converter
 
 %% Converting the EDF File and saving it as a Matlab File
-edf = Edf2Mat('EYE33r10.edf');
+edf2 = Edf2Mat('EYE33r10.edf');
+
+%% Plot the progress of the pupil size
+
+figure();
+plot(edf2.Samples.pa(2, end - 500:end));
