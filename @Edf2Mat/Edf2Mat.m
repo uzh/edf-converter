f80b66571df27689d02bd49584f4a782d6673cf0
Adrian Etter <adrian.etter@econ.uzh.ch>
2013-11-21 14:29:52 +0100

    license added

--- @Edf2Mat/Edf2Mat.m
+++ @Edf2Mat/Edf2Mat.m
@@ -59,6 +59,38 @@ classdef Edf2Mat < handle
     %
     % <a href="matlab:Edf2Mat.about()">Copyright & Info</a>
     
+    % License:
+    %     Copyright (c) 2013, Adrian Etter
+    %     All rights reserved.
+    % 
+    %     Redistribution and use in source and binary forms, with or
+    %     without modification, are permitted provided that the following
+    %     conditions are met:
+    %
+    %     Redistributions of source code must retain the above copyright
+    %     notice, this list of conditions and the following disclaimer.
+    %     Redistributions in binary form must reproduce the above copyright
+    %     notice, this list of conditions and the following disclaimer in
+    %     the documentation and/or other materials provided with the
+    %     distribution. Neither the name of the UNIVERSITY OF ZURICH nor the
+    %     names of its contributors may be used to endorse or promote
+    %     products derived from this software without specific prior
+    %     written permission.
+    % 
+    %     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+    %     CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    %     INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+    %     MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    %     DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+    %     CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    %     SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    %     LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+    %     USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+    %     AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    %     LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    %     ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    %     POSSIBILITY OF SUCH DAMAGE.
+    
     % Event structure from SR-Research:
     %     UINT32 time; /* effective time of event */
     %     INT16 type; /* event type */
