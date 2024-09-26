a003637c2c4ff664062b8ef8143f36fe8aef298b
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2024-09-26 14:06:45 +0200

    read patch version as optional

--- @Edf2Mat/Edf2Mat.m
+++ @Edf2Mat/Edf2Mat.m
@@ -358,7 +358,7 @@ classdef Edf2Mat < handle
             if ~obj.oldProcedure  
                 if ismac
                     [~, version] = unix('sw_vers -productVersion');
-                    version = regexp(version, '(?<major>\d+)\.(?<minor>\d+).(?<patch>\d+)', 'names');
+                    version = regexp(version, '(?<major>\d+)\.(?<minor>\d+)?.?(?<patch>\d+)', 'names');
                     if (str2double(version.major) < 11) 
                        importer = @(varargin)edfimporter_pre11(varargin{:});
                     end                
